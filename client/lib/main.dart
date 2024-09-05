3824ae90ccb5da0ee656a3bad1603837f438ef5f
bingg11 <gyubin235@gmail.com>
2024-09-05 15:45:27 +0900

    데이터 주고 받는 동안의 대기 화면

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_browser/screens/first_screen.dart';
 import 'package:flutter_browser/screens/data_screen.dart';
+import 'package:flutter_browser/screens/loding_screen.dart';
 
 
 void main() {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor:  Color.fromARGB(255, 253, 194, 204)),
         useMaterial3: true,
       ),
-      home: DataScreen()
+      home: LodingScreen()
     );
   }
 }
